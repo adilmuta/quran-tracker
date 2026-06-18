b393118bc63681a1dabb891b89de80c2879f1b71
Mohammad Adil Mutal <adilmuta@amazon.com>
2026-06-18 16:44:16 +0000

    chore: Bump SW cache to v4 to ship page-tracker assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'quran-tracker-v3';
+const CACHE = 'quran-tracker-v4';
 const FILES = ['./index.html','./css/style.css','./js/firebase.js','./js/data.js','./js/app.js','./js/planner.js','./js/hifdh.js','./js/features.js','./js/reports.js'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))) });
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))));
